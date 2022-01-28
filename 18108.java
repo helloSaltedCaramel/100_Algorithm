81779e1edb3673ff20cebcb498374fe58a4ab0c0
JaeYeonCho <94112330+helloSaltedCaramel@users.noreply.github.com>
2022-01-28 15:52:21 +0900

    Create 18108.java

---
+++ 18108.java
@@ -0,0 +1,15 @@
+//1998년생인 내가 태국에서는 2541년생?!
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int y = sc.nextInt();
+       int a = 543;
+
+        System.out.println(y-a);
+
+    }
+}
