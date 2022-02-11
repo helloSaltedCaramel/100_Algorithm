6b393d81576fb59dc6636e2287cbc76d80715b2e
JaeYeonCho <94112330+helloSaltedCaramel@users.noreply.github.com>
2022-02-11 20:11:22 +0900

    [기초-반복실행구조] 정수 1개 입력받아 그 수까지 출력하기1(py)
    input : 4
    
    output:
    0
    1
    2
    3
    4

---
+++ [CU]6075.py
@@ -0,0 +1,26 @@
+#방법 1
+a = int(input())
+n = 0
+while n<=a:
+    print(n)
+    n+=1
+
+#방법 2
+a = int(input())
+n = a 
+while a>=0 :
+  print(n-a)
+  a -= 1
+  
+#방법 3
+#정수형으로 입력받은 뒤 for 반복문으로 0부터 a+1 전까지 print() 함수로 출력해줍니다.
+a = int(input())
+for i in range(0, a+1):
+    print(i)
+   
+  
+
+  
+  
+  
+  
