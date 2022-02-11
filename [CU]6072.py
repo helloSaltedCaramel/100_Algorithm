d8d22aaea682ce51a98b291452b82ab2d70e55de
JaeYeonCho <94112330+helloSaltedCaramel@users.noreply.github.com>
2022-02-11 18:58:35 +0900

    [기초-반복실행구조] 정수 1개 입력받아 카운트다운 출력하기1(설명)(py)

---
+++ [CU]6072.py
@@ -0,0 +1,18 @@
+# 방법 1
+a = int(input())
+
+while True:
+    print(a)
+    
+    a = a-1
+
+    if a ==0:
+        break
+# 방법 2
+a = int(input())
+
+while a!=0:
+    print(a)
+    a = a-1
+        
+        
