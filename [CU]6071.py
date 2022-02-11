7b7b6236e63f588772dc35eb32612dc68d846b07
JaeYeonCho <94112330+helloSaltedCaramel@users.noreply.github.com>
2022-02-11 18:08:56 +0900

    [기초-반복실행구조] 0 입력될 때까지 무한 출력하기(설명)(py)

---
+++ [CU]6071.py
@@ -0,0 +1,22 @@
+
+# 방법1 
+a = int(input())
+while a != 0:
+    print(a)
+    a = int(input())
+
+# 방법2
+while True: 
+  a = int(input()) 
+  if (a == 0): 
+    break 
+    print(a)
+
+# 방법 3
+while True: 
+  a=input() 
+  a=int(a) 
+  if a==0: 
+    break 
+  else: 
+      print(a)
