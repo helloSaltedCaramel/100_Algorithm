7b4d35b3f0275204bdf3b47d5ef64cb4e6b7e86b
JaeYeonCho <94112330+helloSaltedCaramel@users.noreply.github.com>
2022-02-12 01:36:00 +0900

    [기초-종합] 주사위 2개 던지기(설명)(py)
    input : 2  3
    output: 
    1 1
    1 2
    1 3
    2 1
    2 2
    2 3

---
+++ [CU]6080.py
@@ -0,0 +1,6 @@
+a ,b= map(int,input().split())
+for i in range(1,a+1):
+    for j in range(1,b+1):
+        print(i,j)
+        
+#참고 위 코드는 바깥쪽의 i 값이 1부터 n까지 순서대로 바뀌는 각각의 동안에 안쪽의 j 값이 다시 1부터 m까지 변하며 출력되는 코드이다.
