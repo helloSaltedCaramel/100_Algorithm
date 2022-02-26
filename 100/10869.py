1fc76ce3e4cdd99c7fd1e2377f632424bf2bb03d
JaeYeonCho <94112330+helloSaltedCaramel@users.noreply.github.com>
2022-02-26 23:41:28 +0900

    사칙연산

---
+++ 100/10869.py
@@ -0,0 +1,17 @@
+# 방법 1
+a,b = map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b))
+print(a%b)
+
+# 방법 2
+a,b = map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+# 파이썬은 둘로 나누고 떨어지지 않을 경우에는 자동으로 float형으로 출력해준다.
